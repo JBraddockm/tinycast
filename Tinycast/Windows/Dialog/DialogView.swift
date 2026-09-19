7c714bc8c0cb6027c18a7b3c2f2f432d1acb01b8
JBraddockm <jbraddockm@gmail.com>
2026-09-19 14:03:09 +0100

    Merged 563935d706913ab1e78e444687bb846e5bd85ac0

--- Tinycast/Windows/Dialog/DialogView.swift
+++ Tinycast/Windows/Dialog/DialogView.swift
@@ -60,7 +60,7 @@ struct DialogView: View {
         .padding(metrics.spacing.dialogInset)
         .frame(width: width, alignment: .leading)
         .background(Theme.Colors.panelScrim, in: shape)
-        .glassEffect(.regular, in: shape)
+        .frosted(in: shape)
     }
 
     @ViewBuilder private var actions: some View {
